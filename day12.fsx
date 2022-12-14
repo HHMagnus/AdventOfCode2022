6ad2fbbdf03b46aefa8b61c8fe2d2337b096af2a
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2022-12-14 18:23:07 +0100

    day 12 part 1

--- day12.fsx
+++ day12.fsx
@@ -2,27 +2,25 @@ let readLines filePath = System.IO.File.ReadLines(filePath);;
 
 let lines = readLines "day12.txt" |> List.ofSeq
 
-let x = 0
-let y = 0
+let startx = 20
+let starty = 0
 let matrix = lines |> List.map (fun x -> x.ToCharArray() |> List.ofArray |> List.map (fun y -> int y - int 'a'))
-
-type Pair = int * int
-let dict = new System.Collections.Generic.Dictionary<Pair, int>()
 let endd = int 'E' - int 'a'
 
-let rec bst (matrix: int list list) (x, y) length =
- let pos = Pair (x,y)
- if dict.ContainsKey(pos) && dict[pos] <= length then None else
- dict.Remove(pos) |> ignore
- dict.Add(pos, length)
- let field = matrix[x][y]
- //printf "%A %A %A\n" x y field
- if field = endd then Some(length) else
-   [(x+1, y); (x, y+1); (x-1, y); (x, y-1)]
-   |> List.filter (fun (x, y) -> x >= 0 && y >= 0 && x < matrix.Length && y < matrix[0].Length)
-   |> List.filter (fun (x, y) -> (field-1 <= matrix[x][y] && matrix[x][y] <= field+1) || matrix[x][y] = endd)
-   |> List.map (fun p -> bst matrix p (length+1))
-   |> List.filter Option.isSome
-   |> List.min
+let rec bst (queue: (int * int * int) list) (seen: Set<int * int>) =
+ match queue with
+ | [] -> failwith "empty queue"
+ | (x,y,l) :: xs ->
+   let field = matrix[x][y]
+   printf "%A %A\n" (x,y,l) (char (field + int 'a'))
+   if field = endd then l else
+   let rn =
+     [(x+1, y); (x, y+1); (x-1, y); (x, y-1)]
+     |> List.filter (fun (x, y) -> x >= 0 && y >= 0 && x < matrix.Length && y < matrix[0].Length)
+     |> List.filter (fun (x, y) -> (matrix[x][y] <= field+1 && (matrix[x][y] = int 'E' - int 'a' |> not)) || (field = int 'z' - int 'a' && matrix[x][y] = int 'E' - int 'a'))
+     |> List.filter (fun x -> Set.contains x seen |> not)
+   let seenn = rn |> List.fold (fun s a -> Set.add a s) seen
+   bst (xs @ (rn |> List.map (fun (x,y) -> (x,y, l+1)))) seenn
 
-bst matrix (x,y) 0 |> printf "Day 12 (1): %A\n"
+bst [(startx,starty,0)] ([(startx,starty)] |> Set.ofList) |> printf "Day 12 (1): %A\n"
+// I hardcoded S to a for ease
